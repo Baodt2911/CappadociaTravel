b4141cada20b574869123c281753b6941bba9c8e
Baodt2911 <dobao2911bs@gmail.com>
2023-04-27 20:47:47 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif'],
+        Poppins: ['Poppins', 'sans-serif'],
+        Merriweather: ['Merriweather Sans', 'sans-serif']
+      },
+      colors: {
+        grayOpacity: '#CECECE91'
+
+      }
+    },
+  },
+  plugins: [],
+}
+
